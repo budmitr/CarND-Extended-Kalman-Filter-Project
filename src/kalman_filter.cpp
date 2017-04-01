c9985c612d532be5a6602d8458d461a2a3c62f41
Dmitrii Budylskii <d.budylskii@fxpro.com>
2017-04-01 14:44:34 +0300

    Kalman filter predict and update computations done

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -2,6 +2,9 @@
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using std::pow;
+using std::sqrt;
+using std::atan2;
 
 KalmanFilter::KalmanFilter() {}
 
@@ -18,22 +21,51 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  double px = x_[0];
+  double py = x_[1];
+  double vx = x_[2];
+  double vy = x_[3];
+
+  if (px == 0 && py == 0) {
+    px = py = 0.01;
+  }
+
+  VectorXd z_pred = VectorXd(3);
+  z_pred << sqrt(pow(px, 2) + pow(py, 2)),
+            atan2(py, px),
+            (px * vx + py * vy) / (sqrt(pow(px, 2) + pow(py, 2)));
+
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
